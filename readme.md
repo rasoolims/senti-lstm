ad59540c78e0e0635003ddb3241f7c84a942b666
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2016-11-18 19:43:12 -0500

    commit that runs ok with readme

---
+++ readme.md
@@ -0,0 +1,29 @@
+# Requirement
+You need to install [dynet](https://github.com/clab/dynet) library and its python wrapper. Look at its [installation guidelines](http://dynet.readthedocs.io/en/latest/python.html). 
+
+__Note:__ it is usually recommended to use [virtualenv](https://virtualenv.pypa.io/en/stable/) to install python-based libraries.
+
+
+# Current functionalities
+
+# Train
+Train data should contain sentences at each line with labels separated by __tab__. Each word can have translation separated by __|||__ (translation is optional). Labels can be anything. The systems saves the best performing model based the development data.
+
+```
+python -u src/sentilstm.py --train [train-file] --dev [development-file] --outdir [output-directory]--embed [embedding-file(word2vec format))] 
+```
+
+To see all options type:
+```
+python -u src/sentilstm.py
+```
+
+The best model is saved at [output-directory]/model.model and its parameter file as [output-directory]/params.pickle. The model file for each epoch is saved at [output-directory]/model.model_iter_3,[output-directory]/model.model_iter_1, [output-directory]/model.model_iter_2; etc.
+
+# Run Blind Data
+
+Train data should contain sentences at each line (they can have labels as well but the code does not look at them).
+
+```
+python -u src/sentilstm.py  --input [input_file] --output [output_file] --model [model_file] --params [params_file] 
+```
--- src/sentilstm.py
+++ src/sentilstm.py
@@ -1,5 +1,5 @@
 from dynet import *
-import random, sys, os, codecs, pickle, csv
+import os, codecs, pickle,time
 from optparse import OptionParser
 import numpy as np
 
@@ -8,10 +8,10 @@ class SentiLSTM:
     @staticmethod
     def parse_options():
         parser = OptionParser()
-        parser.add_option('--train', dest='train_data', help='train data', metavar='FILE', default='')
-        parser.add_option('--dev', dest='dev_data', help='dev data', metavar='FILE', default='')
-        parser.add_option('--input', dest='input_data', help='input data', metavar='FILE', default='')
-        parser.add_option('--output', dest='output_data', help='output data', metavar='FILE', default='')
+        parser.add_option('--train', dest='train_data', help='train data', metavar='FILE')
+        parser.add_option('--dev', dest='dev_data', help='dev data', metavar='FILE')
+        parser.add_option('--input', dest='input_data', help='input data', metavar='FILE')
+        parser.add_option('--output', dest='output_data', help='output data', metavar='FILE')
         parser.add_option('--params', dest='params', help='Parameters file', metavar='FILE', default='params.pickle')
         parser.add_option('--embed', dest='embed', help='Word embeddings', metavar='FILE')
         parser.add_option('--model', dest='model', help='Load/Save model file', metavar='FILE', default='model.model')
@@ -25,15 +25,16 @@ class SentiLSTM:
 
     def __init__(self, options):
         self.model = Model()
+        self.batchsize = options.batchsize
         self.trainer = AdamTrainer(self.model)
         self.lstm_dims = options.lstm_dims
         self.num_labels = 2
 
         if options.train_data != None:
             labels = set()
-            tf = codecs.open(options.train_data, 'r')
+            tf = codecs.open(os.path.abspath(options.train_data), 'r')
             for row in tf:
-                labels.add(row.split('\t')[1])
+                labels.add(row.strip().split('\t')[1])
             tf.close()
 
             self.rev_labels = list(labels)
@@ -45,7 +46,7 @@ class SentiLSTM:
             to_save_params.append(self.rev_labels)
             to_save_params.append(self.label_dict)
             to_save_params.append(self.num_labels)
-            fp = codecs.open(options.embed, 'r')
+            fp = codecs.open(os.path.abspath(options.embed), 'r')
             fp.readline()
             embed = {line.split(' ')[0]: [float(f) for f in line.strip().split(' ')[1:]] for line in fp}
             fp.close()
@@ -54,8 +55,8 @@ class SentiLSTM:
             self.embed_lookup = self.model.add_lookup_parameters((len(self.word_dict)+1, self.dim))
             self.embed_lookup.set_updated(False)
             for word, i in self.word_dict.iteritems():
-                assert len(embed[word])==self.dim
                 self.embed_lookup.init_row(i, embed[word])
+            self.embed_lookup.init_row(0, [0]*self.dim)
             to_save_params.append(self.word_dict)
             to_save_params.append(self.dim)
             print 'Loaded word embeddings. Vector dimensions:', self.dim
@@ -73,7 +74,6 @@ class SentiLSTM:
             to_save_params.append(self.hid_dim)
             to_save_params.append(self.hid2_dim)
             to_save_params.append(self.hid_inp_dim)
-            print 'writing params'
             with open(os.path.join(options.output, options.params), 'w') as paramsfp:
                 pickle.dump(to_save_params, paramsfp)
             print 'wrote params'
@@ -102,8 +102,165 @@ class SentiLSTM:
         last_hid_dims = self.hid2_dim if self.hid2_dim > 0 else self.hid_dim
         self.O = self.model.add_parameters((self.num_labels, last_hid_dims))
 
+    def build_graph(self, train_lines):
+        errors = []
+        H1 = parameter(self.H1)
+        H2 = parameter(self.H2) if self.H2 != None else None
+        O = parameter(self.O)
+
+        for train_line in train_lines:
+            words,label = train_line.strip().split('\t')
+            label = self.label_dict[label]
+            tokens = words.split()
+            words = []
+            for w in tokens:
+                orig,trans = w,''
+                if '|||' in w:
+                    orig = w[:w.rfind('|||')]
+                    trans = w[w.rfind('|||')+4:]
+
+                if trans in self.word_dict:
+                    words.append(self.word_dict[trans])
+                elif orig in self.word_dict:
+                    words.append(self.word_dict[orig])
+                else:
+                    words.append(0) # unknown translation
+            word_embeddings = [self.embed_lookup[i] for i in words]
+            f_init, b_init = [b.initial_state() for b in self.builders]
+            fw = [x.output() for x in f_init.add_inputs(word_embeddings)]
+            bw = [x.output() for x in b_init.add_inputs(reversed(word_embeddings))]
+
+            input = concatenate([fw[-1],bw[-1]])
+            if H2:
+                r_t = O * rectify(dropout(H2 * (rectify(dropout(H1 * input,0.5))),0.5))
+            else:
+                r_t = O * (rectify(dropout(H1 * input,0.5)))
+            err = pickneglogsoftmax(r_t, label)
+            errors.append(err)
+        return errors
+
+    def train(self, options, best_acc):
+        tf = codecs.open(options.train_data, 'r')
+        instances = []
+        sz = 0
+        i = 0
+        loss = 0
+        start = time.time()
+        for row in tf:
+            instances.append(row)
+            if len(instances)>=self.batchsize:
+                errs = self.build_graph(instances)
+                sum_errs = esum(errs)
+                squared = -sum_errs  # * sum_errs
+                loss += sum_errs.scalar_value()
+                sum_errs.backward()
+                self.trainer.update()
+                sz += len(instances)
+                i+= 1
+                if i%1 == 0:
+                    self.trainer.status()
+                    print 'loss:',loss / sz,'time:',time.time()-start
+                    start = time.time()
+                    sz = 0
+                    loss = 0
+
+                    if options.dev_data != None:
+                        correct = 0
+                        all_dev_num = 0
+                        fp = codecs.open(options.dev_data,'r')
+                        for line in fp:
+                            all_dev_num += 1
+                            sentence,label = line.strip().split('\t')
+                            predicted = self.predict(sentence.strip())
+                            if predicted == label:
+                                correct += 1
+                        acc = float(correct)/all_dev_num
+                        print 'acc', acc
+                        if acc>best_acc:
+                            best_acc = acc
+                            print 'saving best accurary', best_acc
+                            self.model.save(os.path.join(options.output, options.model))
+                errs = []
+                instances = []
+                renew_cg()
+        if len(instances)>=0:
+            errs = self.build_graph(instances)
+            sum_errs = esum(errs)
+            squared = -sum_errs  # * sum_errs
+            loss = sum_errs.scalar_value()
+            sum_errs.backward()
+            self.trainer.update()
+            self.trainer.status()
+            print 'loss:', loss / len(instances), 'time:', time.time() - start
+            start = time.time()
+            instances = []
+            errs = []
+            renew_cg()
+            if options.dev_data != None:
+                correct = 0
+                all_dev_num = 0
+                fp = codecs.open(options.dev_data, 'r')
+                for line in fp:
+                    all_dev_num += 1
+                    sentence, label = line.strip().split('\t')
+                    predicted = self.predict(sentence.strip())
+                    if predicted == label:
+                        correct += 1
+                acc = float(correct) / all_dev_num
+                print 'acc', acc
+                if acc > best_acc:
+                    best_acc = acc
+                    print 'saving best accurary',best_acc
+                    self.model.save(os.path.join(options.output, options.model))
+        return best_acc
+
+    def predict(self, sentence):
+        renew_cg()
+        H1 = parameter(self.H1)
+        H2 = parameter(self.H2) if self.H2 != None else None
+        O = parameter(self.O)
+
+        tokens = sentence.split()
+        words = []
+        for w in tokens:
+            if w in self.word_dict:
+                words.append(self.word_dict[w])
+            else:
+                words.append(0)  # unknown translation
+        word_embeddings = [self.embed_lookup[i] for i in words]
+        f_init, b_init = [b.initial_state() for b in self.builders]
+        fw = [x.output() for x in f_init.add_inputs(word_embeddings)]
+        bw = [x.output() for x in b_init.add_inputs(reversed(word_embeddings))]
+
+        input = concatenate([fw[-1], bw[-1]])
+        if H2:
+            r_t = O * rectify(H2 * (rectify(H1 * input)))
+        else:
+            r_t = O * (rectify(H1 * input))
+        label = np.argmax(r_t.npvalue())
+        return self.rev_labels[label]
+
 
 if __name__ == '__main__':
     (options, args) = SentiLSTM.parse_options()
     senti_lstm = SentiLSTM(options)
-    print senti_lstm.num_labels
+
+    if options.train_data!=None:
+        best_acc = float('-inf')
+        for i in xrange(options.epochs):
+            best_acc = senti_lstm.train(options, best_acc)
+            print 'saving for iteration',i
+            senti_lstm.model.save(os.path.join(options.output, options.model+'_iter_'+str(i)))
+    if options.input_data != None:
+        fp = codecs.open(os.path.abspath(options.input_data), 'r')
+        fw = codecs.open(os.path.abspath(options.output_data), 'w')
+        i = 0
+        start = time.time()
+        for line in fp:
+           sen = line.strip().split('\t')[0]
+           fw.write(sen+'\t'+senti_lstm.predict(sen)+'\n')
+           i += 1
+           if i%100==0: sys.stdout.write(str(i)+'...')
+        fw.close()
+        fp.close()
+        sys.stdout.write('done in '+str(time.time()-start)+'\n')
